48a60803e97c9fc3fbd7c183d84c1b76824f7a7f
Anas Ouadrhiri <anas.ouadrhiri9910@gmail.com>
2021-05-25 13:37:48 +0000

    Add files via upload

---
+++ obj/default/hello_world.d
@@ -0,0 +1,31 @@
+obj/default/hello_world.o: hello_world.c \
+  ../soft_bsp//HAL/inc/sys/alt_stdio.h ../soft_bsp//HAL/inc/sys/alt_irq.h \
+  ../soft_bsp//HAL/inc/nios2.h ../soft_bsp//HAL/inc/alt_types.h \
+  ../soft_bsp/system.h ../soft_bsp/linker.h \
+  ../soft_bsp//HAL/inc/priv/alt_legacy_irq.h ../soft_bsp/system.h \
+  ../soft_bsp//HAL/inc/nios2.h ../soft_bsp//HAL/inc/alt_types.h \
+  ../soft_bsp//HAL/inc/sys/alt_irq.h ../soft_bsp/system.h
+
+../soft_bsp//HAL/inc/sys/alt_stdio.h:
+
+../soft_bsp//HAL/inc/sys/alt_irq.h:
+
+../soft_bsp//HAL/inc/nios2.h:
+
+../soft_bsp//HAL/inc/alt_types.h:
+
+../soft_bsp/system.h:
+
+../soft_bsp/linker.h:
+
+../soft_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../soft_bsp/system.h:
+
+../soft_bsp//HAL/inc/nios2.h:
+
+../soft_bsp//HAL/inc/alt_types.h:
+
+../soft_bsp//HAL/inc/sys/alt_irq.h:
+
+../soft_bsp/system.h:
